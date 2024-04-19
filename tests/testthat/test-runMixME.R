33bda73626c390a24865edd18a5191f872d613e5
pacematt <80705633+pacematt@users.noreply.github.com>
2024-04-19 14:59:54 +0100

    Test tutorials

--- tests/testthat/test-runMixME.R
+++ tests/testthat/test-runMixME.R
@@ -170,6 +170,333 @@ test_that("conditioning a simple single-stock Operating model works", {
 
 })
 
+## ============================================================================#
+## Test conditioning simple mixed fishery Operating Model
+## ============================================================================#
+
+test_that("conditioning a simple mixed fishery Operating model works", {
+  
+  ## load libraries
+  library(FLCore)
+  library(FLFishery)
+  library(mse)
+  library(stockassessment)
+  library(MixME)
+  
+  ## load example data
+  data("mixedfishery_MixME_om")
+  
+  out <- calculateQuotashare(stks = mixedfishery_MixME_om$stks, flts = mixedfishery_MixME_om$flts, verbose = TRUE)
+  mixedfishery_MixME_om$stks <- out$stks
+  mixedfishery_MixME_om$flts <- out$flts
+  
+  out <- stfMixME(mixedfishery_MixME_om,
+                  method = "yearMeans", 
+                  nyears = 20, 
+                  wts.nyears = 3, 
+                  sel.nyears = 3, 
+                  qs.nyears = 3, 
+                  verbose = TRUE)
+  
+  ## Overwrite outputs
+  mixedfishery_MixME_om$stks <- out$stks
+  mixedfishery_MixME_om$flts <- out$flts
+  
+  ## calculate stock numbers in initial year
+  ## initial projection year
+  iy = 2020
+  
+  ## arbitrary effort-based target for each fleet
+  ctrlArgs <- lapply(1:length(mixedfishery_MixME_om$flts), function(x) {
+    list(year = iy,
+         quant = "effort",
+         fishery = names(mixedfishery_MixME_om$flts)[x],
+         value = 1)
+  })
+  ctrlArgs$FCB <- makeFCB(biols = mixedfishery_MixME_om$stks, 
+                          flts = mixedfishery_MixME_om$flts)
+  
+  ## Generate effort-based FLasher::fwd forecast control
+  flasher_ctrl <- do.call(FLasher::fwdControl, ctrlArgs)
+  
+  omfwd <- FLasher::fwd(object    = mixedfishery_MixME_om$stks, 
+                        fishery   = mixedfishery_MixME_om$flts, 
+                        control   = flasher_ctrl)
+  
+  mixedfishery_MixME_om$stks$had@n[,ac(iy)] <- omfwd$biols$had@n[,ac(iy)]
+  mixedfishery_MixME_om$stks$cod@n[,ac(iy)] <- omfwd$biols$cod@n[,ac(iy)]
+  
+  ## convert FLBiol to FLStocks
+  stk_oem <- FLStocks(lapply(mixedfishery_MixME_om$stks, function(x) {
+    
+    ## identify where corresponding catch occurs in fleet structure
+    catch <- sapply(mixedfishery_MixME_om$flts, function(y) which(names(y) %in% name(x)))
+    
+    ## coerce to FLStock
+    xx <- as.FLStock(x, mixedfishery_MixME_om$flts, catch = catch)
+    
+    ## remove excess data
+    stock.n(xx)[] <- NA
+    stock(xx)[]   <- NA
+    harvest(xx)[] <- NA
+    
+    ## return result
+    return(xx)
+  }))
+  
+  ## assemble simulation inputs
+  input <- makeMixME(om = mixedfishery_MixME_om, 
+                     catch_obs = stk_oem, 
+                     management_lag = 0, 
+                     management_type = "fixedC", 
+                     effort_type = "min",
+                     parallel = FALSE)
+  
+  ## Update observation arguments
+  input$oem@args$catch_timing$cod <- 0
+  input$oem@args$catch_timing$had <- 0
+  
+  ## Update management arguments
+  input$ctrl_obj$hcr@args$ctrg$cod <- 1000
+  input$ctrl_obj$hcr@args$ctrg$had <- 1000
+  
+  ## Update simulation arguments
+  input$args$iy
+  
+  ## Update fbar ranges
+  input$args$frange$cod <- c("minfbar" = 2, "maxfbar" = 4)
+  input$args$frange$had <- c("minfbar" = 3, "maxfbar" = 5)
+  
+  ## run the simulation
+  res <- runMixME(om  = input$om, 
+                  oem = input$oem,
+                  ctrl_obj = input$ctrl_obj,
+                  args     = input$args)
+  
+  # ## Check outputs
+  expect_true(all(apply(res$tracking$iterfail, 1, mean) == 0))
+  expect_true(all(res$tracking$optim["convergence",,] == 0))
+  expect_true(all(round(res$tracking$optim["objective",,],5) == 0))
+  expect_true(all(round(apply(res$tracking$quota, c(1,3), sum),5) == 1000))
+  
+  expect_true(all(round(res$tracking$overquota,3)["cod",,,1] == 0))
+  expect_true(all(round(res$tracking$overquota,3)["had",,,1] == 0))
+  
+  expect_true(max(res$tracking$overquota, na.rm = TRUE) < 1e-5)
+  
+  expect_no_error(plot_timeseries_MixME(res, quantity = "ssb"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "fbar"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "catch"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "uptake"))
+  
+  expect_true(all(res$tracking$choke == 1))
+  
+  expect_no_error(plot_timeseries_MixME(res, quantity = "effort", minyr = 2020))
+  
+  ## Add reference points
+  hcrpars <- list(cod = c(Blim = 107000), 
+                  had = c(Blim = 9227))
+  res$ctrl_obj$phcr <- mseCtrl(args   = list(hcrpars = hcrpars))
+  
+  ## Plot risk time-series
+  expect_no_error(plot_timeseries_MixME(res, quantity = "risk"))
+
+})
+
+## ============================================================================#
+## Test running a simple mixed fishery constant catch simulation
+## ============================================================================#
+
+test_that("running a simple mixed fishery constant catch simulation works", {
+  
+  ## load libraries
+  library(FLCore)
+  library(FLFishery)
+  library(mse)
+  library(stockassessment)
+  library(MixME)
+  
+  ## load example data
+  data("mixedfishery_MixME_input")
+  
+  res <- runMixME(om  = mixedfishery_MixME_input$om, 
+                  oem = mixedfishery_MixME_input$oem,
+                  ctrl_obj = mixedfishery_MixME_input$ctrl_obj,
+                  args     = mixedfishery_MixME_input$args)
+  
+  # ## Check outputs
+  expect_true(all(apply(res$tracking$iterfail, 1, mean) == 0))
+  expect_true(all(res$tracking$optim["convergence",,] == 0))
+  expect_true(all(round(res$tracking$optim["objective",,],5) == 0))
+  expect_true(all(round(apply(res$tracking$quota, c(1,3), sum),5) == 1000))
+  
+  expect_true(all(round(res$tracking$overquota,3)["cod",,,1] == 0))
+  expect_true(all(round(res$tracking$overquota,3)["had",,,1] == 0))
+  
+  expect_true(max(res$tracking$overquota, na.rm = TRUE) < 1e-5)
+  
+  expect_no_error(plot_timeseries_MixME(res, quantity = "ssb"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "fbar"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "catch"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "uptake"))
+  
+  expect_true(all(res$tracking$choke == 1))
+  
+  expect_no_error(plot_timeseries_MixME(res, quantity = "effort", minyr = 2020))
+  
+  ## Add reference points
+  hcrpars <- list(cod = c(Blim = 107000), 
+                  had = c(Blim = 9227))
+  res$ctrl_obj$phcr <- mseCtrl(args   = list(hcrpars = hcrpars))
+  
+  ## Plot risk time-series
+  expect_no_error(plot_timeseries_MixME(res, quantity = "risk"))
+  
+})
+
+## ============================================================================#
+## Test running a simple mixed fishery constant fishing mortality simulation
+## ============================================================================#
+
+test_that("running a simple mixed fisheriy constant F simulation works", {
+  
+  ## load libraries
+  library(FLCore)
+  library(FLFishery)
+  library(mse)
+  library(stockassessment)
+  library(MixME)
+  
+  ## load example data
+  data("mixedfishery_MixME_om")
+  
+  out <- calculateQuotashare(stks = mixedfishery_MixME_om$stks, flts = mixedfishery_MixME_om$flts, verbose = TRUE)
+  mixedfishery_MixME_om$stks <- out$stks
+  mixedfishery_MixME_om$flts <- out$flts
+  
+  out <- stfMixME(mixedfishery_MixME_om,
+                  method = "yearMeans", 
+                  nyears = 20, 
+                  wts.nyears = 3, 
+                  sel.nyears = 3, 
+                  qs.nyears = 3, 
+                  verbose = TRUE)
+  ## Overwrite outputs
+  mixedfishery_MixME_om$stks <- out$stks
+  mixedfishery_MixME_om$flts <- out$flts
+  
+  ## initial projection year
+  iy = 2020
+  
+  ## arbitrary effort-based target for each fleet
+  ctrlArgs <- lapply(1:length(mixedfishery_MixME_om$flts), function(x) {
+    list(year = iy,
+         quant = "effort",
+         fishery = names(mixedfishery_MixME_om$flts)[x],
+         value = 1)
+  })
+  ctrlArgs$FCB <- makeFCB(biols = mixedfishery_MixME_om$stks, 
+                          flts = mixedfishery_MixME_om$flts)
+  
+  ## Generate effort-based FLasher::fwd forecast control
+  flasher_ctrl <- do.call(FLasher::fwdControl, ctrlArgs)
+  
+  omfwd <- FLasher::fwd(object    = mixedfishery_MixME_om$stks, 
+                        fishery   = mixedfishery_MixME_om$flts, 
+                        control   = flasher_ctrl)
+  
+  mixedfishery_MixME_om$stks$had@n[,ac(iy)] <- omfwd$biols$had@n[,ac(iy)]
+  mixedfishery_MixME_om$stks$cod@n[,ac(iy)] <- omfwd$biols$cod@n[,ac(iy)]
+  
+  ## convert FLBiol to FLStocks
+  stk_oem <- FLStocks(lapply(mixedfishery_MixME_om$stks, function(x) {
+    
+    ## identify where corresponding catch occurs in fleet structure
+    catch <- sapply(mixedfishery_MixME_om$flts, function(y) which(names(y) %in% name(x)))
+    
+    ## coerce to FLStock
+    xx <- as.FLStock(x, mixedfishery_MixME_om$flts, catch = catch)
+    
+    ## remove excess data
+    stock.n(xx)[] <- NA
+    stock(xx)[]   <- NA
+    harvest(xx)[] <- NA
+    
+    ## return result
+    return(xx)
+  }))
+  
+  input <- makeMixME(om = mixedfishery_MixME_om, 
+                     catch_obs = stk_oem, 
+                     management_lag = 0, 
+                     management_type = "fixedF", 
+                     parallel = FALSE)
+  
+  ## observation error model
+  input$oem@args$catch_timing$cod <- 0
+  input$oem@args$catch_timing$had <- 0
+  
+  input$oem@observations$stk$cod@range[c("minfbar","maxfbar")] <- c(2,4)
+  input$oem@observations$stk$had@range[c("minfbar","maxfbar")] <- c(3,5)
+  
+  ## check estimation module
+  input$ctrl_obj$est@args$estmethod$cod
+  input$ctrl_obj$est@args$estmethod$had
+  
+  ## update HCR
+  input$ctrl_obj$hcr@args$hcrmethod$cod
+  input$ctrl_obj$hcr@args$hcrmethod$had
+  
+  input$ctrl_obj$hcr@args$ftrg$cod <- 0.28  # use MSY f-target
+  input$ctrl_obj$hcr@args$ftrg$had <- 0.353 # use MSY f-target
+  
+  ## Check initial projection year
+  input$args$iy
+  
+  ## Update fbar ranges
+  input$args$frange$cod <- c("minfbar" = 2, "maxfbar" = 4)
+  input$args$frange$had <- c("minfbar" = 3, "maxfbar" = 5)
+  
+  ## run simulation
+  res <- runMixME(om  = input$om, 
+                  oem = input$oem,
+                  ctrl_obj = input$ctrl_obj,
+                  args     = input$args)
+  
+  # ## Check outputs
+  expect_true(all(apply(res$tracking$iterfail, 1, mean) == 0))
+  expect_true(all(round(res$tracking$optim["objective",,],5) == 0))
+  
+  ## Check quota uptake
+  expect_true(all(round(res$tracking$uptake["cod",,,1], 5) == 0))
+  expect_true(all(round(res$tracking$uptake["had",,,1], 5) > 0))
+  
+  ## Check maximum overshoot
+  expect_true(round(max(res$tracking$overquota, na.rm = TRUE),5) == 0)
+  
+  expect_no_error(plot_timeseries_MixME(res, quantity = "ssb"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "fbar", minyr = 2020))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "catch"))
+  expect_no_error(plot_timeseries_MixME(res, quantity = "uptake"))
+  
+  ## Check choke stock
+  expect_true(all(res$tracking$choke == 1))
+  
+  ## Check constant F
+  expect_true(round(mean(res$om$flts$OTB_A@effort[,as.character(2021:2039)]),4) == 1.0976)
+  
+  ## plot fleet properties
+  expect_no_error(plot_timeseries_MixME(res, quantity = "effort", minyr = 2020, maxyr = 2038))
+  
+  ## Add reference points
+  hcrpars <- list(cod = c(Blim = 107000),# 
+                  had = c(Blim = 9227))  # https://doi.org/10.17895/ices.advice.5897
+  res$ctrl_obj$phcr <- mseCtrl(args   = list(hcrpars = hcrpars))
+  
+  ## Plot risk time-series
+  expect_no_error(plot_timeseries_MixME(res, quantity = "risk"))
+  
+})
 
 ## ============================================================================#
 ## Test Error Catching
