c0c25de9aee011f1a0b50ae478d18fd51ae95331
pacematt <80705633+pacematt@users.noreply.github.com>
2026-04-10 17:57:32 +0100

    additional functionality to 'getC'

--- R/helpers_simulation_getC.R
+++ R/helpers_simulation_getC.R
@@ -5,7 +5,7 @@
 # ---
 
 ## function to get total landings / discard numbers for a specified stock
-getC <- function(object, x, sl, summarise = TRUE) {
+getC <- function(object, x, sl, summarise = TRUE, na.rm = FALSE) {
   
   ## loop over each fleet
   res <- lapply(seq_along(object), function(ii) {
@@ -23,17 +23,17 @@ getC <- function(object, x, sl, summarise = TRUE) {
       
       ## return slot aggregated over area
       if (sl %in% c("landings.n","discards.n")) {
-        return(apply(slot(i[[x]], sl), c(1:4,6), sum))
+        return(apply(slot(i[[x]], sl), c(1:4,6), sum, na.rm=na.rm))
       }
       if (sl == "landings.wt") {
-        LF <- sweep(slot(i[[x]], "landings.n"),c(1:4,6),apply(slot(i[[x]],"landings.n"),c(1:4,6),sum),"/")
+        LF <- sweep(slot(i[[x]], "landings.n"),c(1:4,6),apply(slot(i[[x]],"landings.n"),c(1:4,6),sum, na.rm=na.rm),"/")
         LF[is.na(LF)] <- 0
-        return(apply(slot(i[[x]],"landings.wt")*LF,c(1:4,6),sum))
+        return(apply(slot(i[[x]],"landings.wt")*LF,c(1:4,6),sum, na.rm=na.rm))
       }
       if (sl == "discards.wt") {
-        DF <- sweep(slot(i[[x]], "discards.n"),c(1:4,6),apply(slot(i[[x]],"discards.n"),c(1:4,6),sum),"/")
+        DF <- sweep(slot(i[[x]], "discards.n"),c(1:4,6),apply(slot(i[[x]],"discards.n"),c(1:4,6),sum, na.rm=na.rm),"/")
         DF[is.na(DF)] <- 0
-        return(apply(slot(i[[x]],"discards.wt")*DF,c(1:4,6),sum))
+        return(apply(slot(i[[x]],"discards.wt")*DF,c(1:4,6),sum, na.rm=na.rm))
       }
     }
   })
@@ -50,7 +50,7 @@ getC <- function(object, x, sl, summarise = TRUE) {
 }
 
 ## function to get total landings, discards weights for a specified stock
-getCW <- function(object, x, sl = "landings", summarise = TRUE) {
+getCW <- function(object, x, sl = "landings", summarise = TRUE, na.rm=TRUE) {
   
   ## loop over each fleet
   res <- sapply(seq_along(object), function(ii) {
@@ -68,7 +68,7 @@ getCW <- function(object, x, sl = "landings", summarise = TRUE) {
       
       ## return slot aggregated over area
       return(apply(slot(i[[x]], paste0(sl,".n")) * slot(i[[x]], paste0(sl,".wt")), 
-                   c(2:4,6), sum, na.rm=TRUE))
+                   c(2:4,6), sum, na.rm=na.rm))
     }
   },simplify = FALSE, USE.NAMES = TRUE)
   
